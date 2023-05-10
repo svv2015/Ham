4bd389d3b4db77a5d29f928354e61734903d2fe7
Svv2015 <svv2015@gmail.com>
2023-05-10 09:48:31 +0300

    my fist cormit

--- js/carousel.js
+++ js/carousel.js
@@ -1,47 +1,47 @@
 let slider = 1;
-                function showSlider(item) {
-                    const ollSlids = document.querySelectorAll('.item-carousel-info');
-                    const ollSlidsPhoto = document.querySelectorAll('.item');
+function showSlider(item) {
+    const ollSlids = document.querySelectorAll('.item-carousel-info');
+    const ollSlidsPhoto = document.querySelectorAll('.item');
 
-                    if (item > ollSlids.length) {
-                        slider = 1;
-                    }
-                    if (item < 1) {
-                        slider = ollSlids.length
-                    }
-                    console.log(slider);
-                }
-                function plus() {
-                    const ollSlids = document.querySelectorAll('.item-carousel-info');
-                    const ollSlidsPhoto = document.querySelectorAll('.item');
+    if (item > ollSlids.length) {
+        slider = 1;
+    }
+    if (item < 1) {
+        slider = ollSlids.length
+    }
+    console.log(slider);
+}
+function plus() {
+    const ollSlids = document.querySelectorAll('.item-carousel-info');
+    const ollSlidsPhoto = document.querySelectorAll('.item');
 
-                    ollSlids[slider - 1].classList.toggle('display-none')
-                    ollSlidsPhoto[slider - 1].classList.toggle('totop')
+    ollSlids[slider - 1].classList.toggle('display-none')
+    ollSlidsPhoto[slider - 1].classList.toggle('totop')
 
-                    showSlider(slider += 1)
-                    ollSlids[slider - 1].classList.toggle('display-none')
-                    ollSlidsPhoto[slider - 1].classList.toggle('totop')
+    showSlider(slider += 1)
+    ollSlids[slider - 1].classList.toggle('display-none')
+    ollSlidsPhoto[slider - 1].classList.toggle('totop')
 
 
-                }
-                function minus() {
-                    const ollSlids = document.querySelectorAll('.item-carousel-info');
-                    const ollSlidsPhoto = document.querySelectorAll('.item');
-                    ollSlidsPhoto[slider - 1].classList.toggle('totop')
+}
+function minus() {
+    const ollSlids = document.querySelectorAll('.item-carousel-info');
+    const ollSlidsPhoto = document.querySelectorAll('.item');
+    ollSlidsPhoto[slider - 1].classList.toggle('totop')
 
-                    ollSlids[slider - 1].classList.toggle('display-none')
-                    showSlider(slider -= 1)
-                    ollSlids[slider - 1].classList.toggle('display-none')
-                    ollSlidsPhoto[slider - 1].classList.toggle('totop')
-                }
-                function clickOnSlider(i) {
-                    const ollSlids = document.querySelectorAll('.item-carousel-info');
-                    const ollSlidsPhoto = document.querySelectorAll('.item');
-                    
-                    ollSlidsPhoto[slider - 1].classList.toggle('totop')
-                    
-                    ollSlids[slider - 1].classList.toggle('display-none')
-                    slider = i;
-                    ollSlids[slider - 1].classList.toggle('display-none')
-                    ollSlidsPhoto[slider - 1].classList.toggle('totop')
-                }
+    ollSlids[slider - 1].classList.toggle('display-none')
+    showSlider(slider -= 1)
+    ollSlids[slider - 1].classList.toggle('display-none')
+    ollSlidsPhoto[slider - 1].classList.toggle('totop')
+}
+function clickOnSlider(i) {
+    const ollSlids = document.querySelectorAll('.item-carousel-info');
+    const ollSlidsPhoto = document.querySelectorAll('.item');
+
+    ollSlidsPhoto[slider - 1].classList.toggle('totop')
+
+    ollSlids[slider - 1].classList.toggle('display-none')
+    slider = i;
+    ollSlids[slider - 1].classList.toggle('display-none')
+    ollSlidsPhoto[slider - 1].classList.toggle('totop')
+}
